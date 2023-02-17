dfec9822b15d12cf397a6f468c58efe4a1ba0b2d
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-02-17 10:01:31 +0000

    'use strict' mode

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,24 +1,26 @@
 // Split string by the first occurrence of separator
+'use strict';
 
-Parseip = (i) => {
+const Parseip = (i) => {
 
-  a = [];
+  const a = [];
 
   if (i === '') return;
 
   else {
-    B = i.split('.');
+    const B = i.split('.');
 
     if (B.length != 4) return;
-    j = 0;
+    let j = 0;
 
     for (const b of B) {
+
       a[j] = parseInt(b);
       if (isNaN(a[j])) return;
       j++;
+
     }
   }
-  
   return a;
 };
 
