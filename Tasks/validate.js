7a8558ced4c34f951f7838b03c686fd26fe71f44
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-02-18 16:32:05 +0000

    1 if замість 5

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -2,20 +2,22 @@
 
 const isValidate = (T) => {
 
-  if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
+  if (!T || T === '' 
+    || typeof T !== 'string' 
+    || T.length === 0 
+    || !T.includes(' ')
+  ) 
+  return false;
 
     for (C of T) {
+
       if (C === ' ') continue;
 
       if (
         C.toLowerCase().charCodeAt(0) >= 97 &&
         C.toLowerCase().charCodeAt(0) <= 122
       ) 
-      
+
       {
       } 
       else {
