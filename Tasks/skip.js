793b44f93f039f4c704317e133163d43f1817eef
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-02-23 07:03:06 +0000

    skip: відступи.

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,15 +1,23 @@
 // Return an remove without listed values
 
 Skip = (T, ...Remove) => {
+
   x = 0;
+
   for (C of T) {
+
     for (X of Remove) {
+
       if (C === X) {
+
         T.splice(x, 1);
+
       }
     }
+
     x++;
   }
+  
   return T;
 };
 
