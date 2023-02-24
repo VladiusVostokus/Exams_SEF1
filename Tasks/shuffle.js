5e133cb035f7813a87fe784662031f49af0616e8
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-02-24 08:59:38 +0000

    shuffle: відступи.

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -1,11 +1,13 @@
 // Shuffle an array
 
 let Shuffle = (...List) => {
+
   const [arr] = List;
+
   arr.sort(() => Math.random() - 0.5);
-  {
-    return arr;
-  }
+
+  return arr;
+  
 };
 
 require('../Tests/shuffle.js')(Shuffle);
