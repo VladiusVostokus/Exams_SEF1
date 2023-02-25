ce5ecbed09eaa678f1ea14d0959258d965b63142
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-02-25 16:36:20 +0000

    section: 'use strict'.

--- Tasks/section.js
+++ Tasks/section.js
@@ -1,10 +1,11 @@
 // Split string into array by the first occurrence of separator
+'use strict'
 
-SectionString = (s, t) => {
+const SectionString = (s, t) => {
 
-  i = s.indexOf(t);
+  const i = s.indexOf(t);
 
-  if (i < 0 || t == '') return [s, ''];
+  if (i < 0 || t === '') return [s, ''];
 
   else return [s.slice(0, i), s.slice(i + t.length)];
   
