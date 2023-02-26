ab51e452de08227f27247ad6cdede6744816d05f
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-02-26 10:02:34 +0000

    sample: відступи.

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,8 +1,11 @@
 // Get one random element from an array
 
 const SAMPLE = (arr) => {
+
   arr = arr[Math.floor(Math.random() * arr.length)];
+  
   return arr;
+
 };
 
 require('../Tests/sample.js')(SAMPLE);
