f53926b3db6815234e0c11581f3e012a07cbf04f
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-03-01 16:28:23 +0000

    random: присвоєння обчислень у проміжні змінні.

--- Tasks/random.js
+++ Tasks/random.js
@@ -3,8 +3,12 @@
 Random = (min, max) => {
 
   if (max === undefined) return min;
+
+  const randomValue = min + Math.random() * (max - min);
+
+  const result = Math.floor(randomValue);
     
-  return min + Math.floor(Math.random() * (max - min));
+  return result;
   
 };
 
