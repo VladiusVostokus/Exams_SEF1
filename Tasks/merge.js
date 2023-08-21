c3d117ae40389e745ebd3a8bb11575859149fb77
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-08-21 13:43:57 +0000

    merge: об'єднання об'єктів через spread оператор.

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -2,17 +2,9 @@
 
 const merge_two_objects = (object_1, object_2) => {
 
-  object_3 = [{}][0];
-
-  for (attribute_name in object_1) {
-    object_3[attribute_name] = object_1[attribute_name];
-  }
-
-  for (attribute_name in object_2) {
-    object_3[attribute_name] = object_2[attribute_name];
-  }
-
-  return object_1, object_2, object_3;
+  object_3 = {...object_1,...object_2};
+  
+  return object_3;
   
 };
 
